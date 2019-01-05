36ca94e91d945389ddd8a4aa1ccaf9c24f674056
Gyusun Yeom <omniavinco@gmail.com>
2019-01-05 15:36:09 +0900

    Setup for load css

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,9 +1,13 @@
 import HtmlPlugin from "html-webpack-plugin";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
 import path from "path";
 import webpack from "webpack";
 
+const devMode = process.env.NODE_ENV !== "production";
+
 export default function() {
 	return {
+		devtool: "source-map",
 		entry: path.join(__dirname, "src", "index.tsx"),
 		mode: devMode ? "development" : "production",
 		module: {
@@ -11,6 +15,17 @@ export default function() {
 				{
 					include: /\.tsx?$/,
 					loaders: ["ts-loader"],
+				},
+				{
+					include: /\.css$/,
+					loaders: [
+						devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+						"css-loader",
+					],
+				},
+				{
+					include: /\.(woff|woff2|eot|ttf|otf|png|svg)$/,
+					loader: "file-loader",
 				},
 			],
 		},
@@ -21,6 +36,11 @@ export default function() {
 			new HtmlPlugin({
 				template: "src/index.html",
 			}),
+			...(!devMode ? [
+				new MiniCssExtractPlugin({
+					filename: "styles.css",
+				}),
+			] : []),
 		],
 		resolve: {
 			extensions: [
