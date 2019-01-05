362579e176b28fd50b7b7580bc20d94a6ed51cb4
Gyusun Yeom <omniavinco@gmail.com>
2019-01-05 16:12:33 +0900

    Add routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,26 @@
 import React from "react";
 import { render } from "react-dom";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import "semantic-ui-css/semantic.min.css";
+import { Container, Menu } from "semantic-ui-react";
+import Dashboard from "./pages/dashboard";
 
 class Application extends React.Component {
 	public render() {
-		return <p>Hello, World!</p>;
+		return <Router>
+			<div>
+				<Menu fixed="top">
+					<Container>
+						<Menu.Item as={Link} header to="/">
+							Logo
+						</Menu.Item>
+					</Container>
+				</Menu>
+				<Container style={{ marginTop: "5em" }}>
+					<Route exact path="/" component={Dashboard} />
+				</Container>
+			</div>
+		</Router>;
 	}
 }
 
---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Header } from "semantic-ui-react";
+
+export default class Dashboard extends React.Component {
+	public render() {
+		return <Header>Dashboard</Header>;
+	}
+}
